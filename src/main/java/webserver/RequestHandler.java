ee9f5e62d99350873449bb778512f90efbab26ea
belselios@gmail.com <belselios@gmail.com>
2018-10-10 23:47:21 +0900

    4장 요구사항7 완료

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -112,10 +112,27 @@ public class RequestHandler extends Thread {
                 responseBody(dos, body);
 
 
+            } else if(url.endsWith(".css")) {
+                DataOutputStream dos = new DataOutputStream(out);
+                byte[] body = Files.readAllBytes(new File("./webapp"+url).toPath());
+                response200CssHeader(dos, body.length);
+                responseBody(dos, body);
+
             } else {
                 responseResource(out, url);
             }
 
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void response200CssHeader(DataOutputStream dos, int lengthOfBodyContent) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/css \r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.error(e.getMessage());
         }
